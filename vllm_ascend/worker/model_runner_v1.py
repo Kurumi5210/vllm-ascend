59fa18744066d3b69b1bb9880376eff7ef3f7b96
weiguihua2 <weiguihua2@huawei.com>
2026-03-17 14:35:02 +0800

    [fix]fix model_runner
    Signed-off-by: weiguihua2 <weiguihua2@huawei.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -86,7 +86,7 @@ from vllm.v1.worker.utils import AttentionGroup
 
 from vllm_ascend.ascend_config import get_ascend_config
 from vllm_ascend.attention.attention_v1 import AscendAttentionState
-from vllm_ascend.attention.utils import AscendCommonAttentionMetadata, using_paged_attention, get_dcp_local_seq_lens
+from vllm_ascend.attention.utils import AscendCommonAttentionMetadata, using_paged_attention
 
 # yapf conflicts with isort for this block
 # yapf: disable
