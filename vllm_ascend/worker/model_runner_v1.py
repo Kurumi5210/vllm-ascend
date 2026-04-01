e27e6ec8c79b29d9e056cc0af4c691b2a4193cc8
chenxiao <Jaychou1620@Gmail.com>
2026-04-01 11:28:28 +0800

    [debug] bugfix
    Signed-off-by: chenxiao <Jaychou1620@Gmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -2262,6 +2262,7 @@ class NPUModelRunner(GPUModelRunner):
         remove_lora: bool = True,
         is_graph_capturing: bool = False,
         num_active_loras: int = 0,
+        num_dycp_reqs: int = 0,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         # only support eager mode and piecewise graph now
         assert cudagraph_runtime_mode is None or cudagraph_runtime_mode.valid_runtime_modes()
